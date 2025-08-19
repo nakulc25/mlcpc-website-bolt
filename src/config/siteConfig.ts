9d01ede1c79367aa952a727ffa01eba1a2c694aa
Nakul Chhibbar <nakulchhibbar@gmail.com>
2025-08-20 00:14:16 +0530

    Updated siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -63,7 +63,7 @@ export interface SiteConfig {
 export const siteConfig: SiteConfig = {
   firm: {
     name: "Mall & Chhibbar Legal Professional Corporation",
-    shortName: "M&C",
+    shortName: "M and C",
     description: "Serving Toronto and the GTA"
   },
   contact: {
